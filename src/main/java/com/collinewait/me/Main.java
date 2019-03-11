3437f5f916b28dadaa7a74bcaa29377223f9ffcd
collinewait <collinewait17@gmail.com>
2019-03-11 22:25:22 +0300

    add spark-template-handlebars

--- src/main/java/com/collinewait/me/Main.java
+++ src/main/java/com/collinewait/me/Main.java
@@ -1,9 +1,14 @@
 package com.collinewait.me;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+
+        get("/", (req, res) ->  new ModelAndView(null, "index.hbs"),
+                new HandlebarsTemplateEngine());
     }
 }
